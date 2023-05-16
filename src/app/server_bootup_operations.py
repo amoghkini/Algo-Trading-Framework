09a620d5cd7a946d8b4f06fb036eb112e40fc3b9
Amogh Kini <amoghkini@gmail.com>
2023-05-17 03:33:45 +0530

    Adding server bootup operations

--- src/app/server_bootup_operations.py
+++ src/app/server_bootup_operations.py
@@ -1,6 +1,6 @@
 from algo.algo_methods import AlgoMethods
 
 def server_bootup_operations():
-    print("Amogh is inside server bootup menu")
-    #AlgoMethods.save_algo_data()    
+    print("Going to perform the server bootup operations")
+    AlgoMethods.save_algo_data()    
     
